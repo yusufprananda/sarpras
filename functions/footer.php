d0c69b6cb9a927262d2830b22396834e09b87ae0
Yusuf Prananda <nandaarankaru@gmail.com>
2020-02-13 10:49:33 +0700

    Mengganti footer.

--- functions/footer.php
+++ functions/footer.php
@@ -2,7 +2,7 @@
 <footer class="cFooter text-white">
     <div class="d-flex flex-row justify-content-center">
         <div class="p-3">
-            <p class="m-0">Copyright © <?= date("Y") ?> MYP | All Rights Reserved</p>
+            <p class="m-0 text-center">Copyright © <?= date("Y") ?> <br>Tim BPTIK Universitas Ngudi Waluyo</p>
         </div>
     </div>
 </footer>
